a8f6a99b58439418a4a8fb16ca4ab9cc84e9d0a7
Martin Elsner Ramos <martin.elsner@gmail.com>
2015-09-23 23:04:19 -0300

    Fixed paths

--- default.nix
+++ default.nix
@@ -5,19 +5,28 @@ buildPythonPackage {
     name = "impurePythonEnv";
     buildInputs = [
         stdenv
-        python35
-        python35Packages.ipython
-        python35Packages.pyqt5
-        #python35Packages.pyqtgraph
-        python35Packages.pyserial
-        python35Packages.virtualenv
+        python34
+        python34Packages.ipython
+        python34Packages.numpy
+        python34Packages.pyqt5
+        #python34Packages.pyqtgraph
+        python34Packages.pyserial
+        python34Packages.sip_4_16
+        python34Packages.virtualenv
         qt5Full
+        qt5.declarative
+        #qt5.quick1
+        qt5.quickcontrols
+        #protobuf3_0
     ];
     src = null;
     # When used as `nix-shell --pure`
     shellHook = ''
         unset http_proxy
         export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
+        export LD_LIBRARY_PATH="${qt5.declarative}/lib:${qt5.quick1}/lib:${qt5.quickcontrols}/lib"
+        export PYTHONPATH="${python34Packages.numpy}/lib/python3.4/site-packages:${python34Packages.pyqt5}/lib/python3.4/site-packages:${python34Packages.pyserial}/lib/python3.4/site-packages:${python34Packages.sip_4_16}/lib/python3.4/site-packages:$PYTHONPATH"
+        #echo "${protobuf3_0}"
     '';
     # used when building environments
     extraCmds = ''
